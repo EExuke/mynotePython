da0fc55ddd5794c3eecc310f20b2ea339074e0b1
EExuke <EExuke>
2020-01-17 15:11:02 +0800

    Cloud Studio push: * 2_list: 5_tuples.py * 3_if: 1_ ,2_ , 3_;

--- 3_if/1_TrueFlse.py
+++ 3_if/1_TrueFlse.py
@@ -4,20 +4,28 @@
 #
 # --------------------------------------------------------------------------
 #     AUTHOR                   : EExuke
-#     FILE NAME                : 1_names.py
+#     FILE NAME                : 5_tuples.py
 #     FILE DESCRIPTION         : Python file
-#     FIRST CREATION DATE      : 2020/01/06
+#     FIRST CREATION DATE      : 2020/01/17
 # --------------------------------------------------------------------------
 #     Version                  : 1.0
-#     Last Change              : 2020/01/06
+#     Last Change              : 2020/01/17
 ## ************************************************************************** ##
 #
-names = ['XuKe', 'GuJiaLei', 'ChengYiJie']
-for name in names :
-    print (name)
+# 5_1 & 5_2 #
+car = 'Jili'
+print(car == 'Auto')
+print(car == 'Jili')
 
-print ("\n")
+print((car.lower() == 'jili') and (car != 'Auto'))
+print((car.lower() == 'bmw') or (car != 'Auto'))
 
-for name in names :
-    message = "hello " + name + ", good to see you!"
-    print (message)
+numbs = range(1,10)
+numb = 7
+if numb in numbs :
+    print(str(numb) + " in numb list \n")
+
+numb = 77
+if numb not in numbs :
+    print(str(numb) + " not in numbs list \n")
+
